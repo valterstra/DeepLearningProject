00caa72ac8a45918abdd6320d0d1b7bcc294f15c
Valter Strandler <valterst@kth.se>
2025-05-12 09:26:38 +0200

    Added functionality to induce noise in labels on CIFAR

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, random_split
 import torchvision
-from torchvision import transforms
+from torchvision import transforms,datasets
 import numpy as np
 import random
 import matplotlib.pyplot as plt
@@ -16,6 +16,86 @@ def set_seed(seed=42):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+
+
+class CIFAR10Noisy(datasets.CIFAR10):
+    """
+    CIFAR-10 dataset with optional symmetric or asymmetric label noise.
+    """
+    def __init__(self, root, train=True, transform=None, download=False,
+                 noise_rate=0.0, asym=False, seed=0):
+        super().__init__(root, train=train, transform=transform, download=download)
+        np.random.seed(seed)
+        self.noise_rate = noise_rate
+        self.asym = asym
+        self._inject_noise()
+
+    def _inject_noise(self):
+        if self.asym:
+            source = [9, 2, 3, 5, 4]
+            target = [1, 0, 5, 3, 7]
+            for s, t in zip(source, target):
+                idx = np.where(np.array(self.targets) == s)[0]
+                n_noisy = int(self.noise_rate * len(idx))
+                noisy_idx = np.random.choice(idx, n_noisy, replace=False)
+                for i in noisy_idx:
+                    self.targets[i] = t
+        elif self.noise_rate > 0:
+            for i in range(len(self.targets)):
+                if np.random.rand() < self.noise_rate:
+                    orig = self.targets[i]
+                    choices = list(range(10))
+                    choices.remove(orig)
+                    self.targets[i] = np.random.choice(choices)
+
+
+def get_noisy_cifar10_loaders(root='./data', noise_rate=0.0, asym=False,
+                               batch_size=64, val_size=1000,
+                               use_random_crop=False, use_random_flip=False, use_normalization=False,
+                               num_workers=4, seed=42):
+
+    transform_list = []
+    if use_random_crop:
+        transform_list.append(transforms.RandomCrop(32, padding=4))
+    if use_random_flip:
+        transform_list.append(transforms.RandomHorizontalFlip())
+    transform_list.append(transforms.ToTensor())
+    if use_normalization:
+        transform_list.append(transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                                   (0.2023, 0.1994, 0.2010)))
+    train_transform = transforms.Compose(transform_list)
+
+    base_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    ]) if use_normalization else transforms.ToTensor()
+
+    if noise_rate > 0:
+        full_train = CIFAR10Noisy(root=root, train=True, download=True,
+                                   transform=train_transform,
+                                   noise_rate=noise_rate, asym=asym, seed=seed)
+    else:
+        full_train = torchvision.datasets.CIFAR10(root=root, train=True, download=True,
+                                                  transform=train_transform)
+
+    total = len(full_train)
+    val_size = min(val_size, total)
+    train_size = total - val_size
+    generator = torch.Generator().manual_seed(seed)
+    train_set, val_set = random_split(full_train, [train_size, val_size], generator=generator)
+
+    test_set = torchvision.datasets.CIFAR10(root=root, train=False, download=True,
+                                            transform=base_transform)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True,
+                              num_workers=num_workers, pin_memory=True)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False,
+                            num_workers=num_workers, pin_memory=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False,
+                             num_workers=num_workers, pin_memory=True)
+
+    return train_loader, val_loader, test_loader
 
 
 def load_data(batch_size=64, val_size=1000, use_random_crop=False, use_random_flip=False, use_normalization=False):
@@ -54,7 +134,7 @@ def load_data(batch_size=64, val_size=1000, use_random_crop=False, use_random_fl
 
 
 class CIFAR10Model(nn.Module):
-    def __init__(self, use_dropout=True, dropout_rate=0.2, use_batchnorm=False, use_stride_downsampling=False, use_gap=False):
+    def __init__(self, use_dropout=True, dropout_rate=0.2, use_batchnorm=False, use_stride_downsampling=False):
         super().__init__()
         self.relu = nn.ReLU()
         self.use_dropout = use_dropout
@@ -82,27 +162,16 @@ class CIFAR10Model(nn.Module):
         self.bn3_2 = nn.BatchNorm2d(256) if use_batchnorm else nn.Identity()
         self.pool3 = nn.Identity() if use_stride_downsampling else nn.MaxPool2d(2, 2)
 
-        self.use_gap = use_gap
-        if self.use_gap:
-            self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-            self.classifier = nn.Linear(256, 10)
-        else:
-            self.flatten = nn.Flatten()
-            self.fc1 = nn.Linear(256 * 4 * 4, 128)
-            self.fc2 = nn.Linear(128, 10)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(256 * 4 * 4, 128)
+        self.fc2 = nn.Linear(128, 10)
 
-        layers_to_init = [
+        for layer in [
             self.conv1_1, self.conv1_2,
             self.conv2_1, self.conv2_2,
             self.conv3_1, self.conv3_2,
-        ]
-
-        if self.use_gap:
-            layers_to_init.append(self.classifier)
-        else:
-            layers_to_init.extend([self.fc1, self.fc2])
-
-        for layer in layers_to_init:
+            self.fc1, self.fc2
+        ]:
             if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
                 nn.init.kaiming_uniform_(layer.weight, nonlinearity='relu' if layer != self.fc2 else 'linear')
                 nn.init.zeros_(layer.bias)
@@ -123,29 +192,12 @@ class CIFAR10Model(nn.Module):
         x = self.pool3(x)
         x = self.dropout(x)
 
-        if self.use_gap:
-            x = self.global_avg_pool(x)
-            x = torch.flatten(x, 1)
-            x = self.classifier(x)
-        else:
-            x = self.flatten(x)
-            x = self.relu(self.fc1(x))
-            x = self.dropout(x)
-            x = self.fc2(x)
+        x = self.flatten(x)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
         return x
 
-class LabelSmoothingCrossEntropy(nn.Module):
-    def __init__(self, smoothing=0.1):
-        super().__init__()
-        self.smoothing = smoothing
-        self.confidence = 1.0 - smoothing
-
-    def forward(self, x, target):
-        log_probs = nn.functional.log_softmax(x, dim=-1)
-        true_dist = torch.zeros_like(log_probs)
-        true_dist.fill_(self.smoothing / (x.size(1) - 1))
-        true_dist.scatter_(1, target.unsqueeze(1), self.confidence)
-        return torch.mean(torch.sum(-true_dist * log_probs, dim=-1))
 
 def train_one_epoch(model, dataloader, optimizer, criterion, device):
     model.train()
@@ -208,35 +260,44 @@ def main():
     use_random_crop = False #this is the translation agumentation that they are talking about
     use_random_flip = False
     use_normalization = False
-    use_label_smoothing = True
-    use_gap = True
 
     use_dropout = True
     dropout_rate = 0.3
 
     use_batchnorm = False
-    use_stride_downsampling = True  # set False to use traditional max pooling
+    use_stride_downsampling = False  # set False to use traditional max pooling
 
     use_scheduler = False #this gives the cosine with warmup
     use_warm_restarts = False  #this is active if we want the cosine with restarts
     warmup_epochs = 5
-    total_epochs = 100
+    total_epochs = 15
     cosine_anneal_epochs = total_epochs - warmup_epochs
 
-    train_loader, val_loader, test_loader = load_data(
-        batch_size=64,
-        val_size=1000,
-        use_random_crop=use_random_crop,
-        use_random_flip=use_random_flip,
-        use_normalization=use_normalization
-    )
+    noisy_labels = False
+    noise_rate = 0.5
+    asym = False
+
+    if noisy_labels:
+        train_loader, val_loader, test_loader = get_noisy_cifar10_loaders(
+            root='./data', noise_rate=noise_rate, asym=asym,
+            batch_size=64, val_size=1000,
+            use_random_crop=use_random_crop, use_random_flip=use_random_flip,
+            use_normalization=use_normalization, num_workers=4, seed=42
+        )
+    else:
+        train_loader, val_loader, test_loader = load_data(
+            batch_size=64,
+            val_size=1000,
+            use_random_crop=use_random_crop,
+            use_random_flip=use_random_flip,
+            use_normalization=use_normalization
+        )
 
     model = CIFAR10Model(
         use_dropout=use_dropout,
         dropout_rate=dropout_rate,
         use_batchnorm=use_batchnorm,
-        use_stride_downsampling=use_stride_downsampling,
-        use_gap=use_gap
+        use_stride_downsampling=use_stride_downsampling
     ).to(device)
 
     optimizer = optim.AdamW(
@@ -263,10 +324,7 @@ def main():
     else:
         scheduler = None
 
-    if use_label_smoothing:
-        criterion = LabelSmoothingCrossEntropy(smoothing=0.1)
-    else:
-        criterion = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
 
     train_acc, val_acc = [], []
     train_loss, val_loss = [], []
